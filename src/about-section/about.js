86d6f9c95f0dc2d7bd233d44c7e710ee565660a2
Tung Khuat <tungkhuat09@gmail.com>
2020-06-03 14:34:15 +0300

    Update about section information

--- src/about-section/about.js
+++ src/about-section/about.js
@@ -1,29 +1,61 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { ThemeContext } from '../theme/theme-context';
+import { Title, Wrapper, HighlightedLink } from '../styled-components/basic-components';
+import data from '../../data/about-info.json';
 import SideTitleSplitSection from '../section-template/side-title-split-section';
-import { Title } from '../styled-components/basic-components'
+import HexagonImage from '../image-display/hexagon-image';
+import TechListDisplay from './tech-list-display';
 
-const AboutContentTitle = styled(Title)`
-    padding: 20px 0;
+
+const AboutContentHeader = styled(Title)`
+    padding: 25px 0;
 `
+const CtaLink = styled(HighlightedLink)`
+    margin: 10px 0;
+    display: block;
+`
+
 
 export default function AboutSection() {
     const themeContext = useContext(ThemeContext);
     
     return(
-        <SideTitleSplitSection
-            title="About"
-            titleBackground={ themeContext.theme.highlight }
-            titleTextColor={ themeContext.theme.background }
-            underlineColor={ themeContext.theme.background }
-            navTextColor = { themeContext.theme.foreground }
-        >
-            <AboutContentTitle>Placeholder</AboutContentTitle>
-            <p>
-                Starting with basic websites several years ago I found myself working as an Art Director for complex projects with a holistic approach soon. Visually appealing designs, subtle details and brand guidelines combined to innovative interfaces across various touch points became my daily companion.
-            </p>
-            
-        </SideTitleSplitSection>            
+            <SideTitleSplitSection
+                title="About"
+                titleBackground={ themeContext.theme.highlight }
+                titleFontColor={ themeContext.theme.lightest }
+                underlineColor={ themeContext.theme.lightest }
+                navFontColor = { themeContext.theme.darkest }
+                middleElement = {<HexagonImage src={"./images/profile-pic.jpg"} />}
+            >
+                <AboutContent/>
+            </SideTitleSplitSection>            
     )
 };
+
+function AboutContent() {
+    return(
+        <Wrapper>
+            {
+                data && (
+                    data.map((section) => (
+                        <div>
+                            <AboutContentHeader>{section.header}</AboutContentHeader>
+                            <h4>{ section.subHeader }</h4>
+                            <p>{ section.paragraph }</p>
+                            { 
+                                section.callToAction && section.callToAction.length > 0 &&(
+                                    section.callToAction.map((cta) => (
+                                        <CtaLink href={ cta.action }>{ cta.label }</CtaLink>
+                                    )) 
+                                )
+                            }
+                        </div>
+                    ))
+                )
+            }
+            <TechListDisplay />
+        </Wrapper>
+    )
+}
---
+++ src/about-section/tech-list-display.js
@@ -0,0 +1,131 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { ThemeContext } from '../theme/theme-context';
+import { Wrapper } from '../styled-components/basic-components';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const TechListContainer = styled(Wrapper)`
+    width: 100%;
+    margin: 30px 0;
+`
+const TechGroup = styled.div`
+    min-width: 200px;
+    width: 100%;
+`
+const TechIconWrap = styled.div`
+    min-width: 200px;
+`
+const TechTitleWrap = styled.div`
+    width: 100%;
+    span:not(:first-of-type):before {
+        content: ', ';
+    }
+    font-weight: bold;
+    margin-bottom: 10px;
+`
+const TechGroupDetails = styled.div`
+    display: block;
+`
+const TechIcon = styled.div`
+    background-image: url(${({backgroundImage}) => backgroundImage});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    width: 48px;
+    height: 48px;
+    display: inline-block;
+    margin-right: 7px;
+    &:hover:after {
+        content: '${({iconLabel}) => iconLabel}';
+        display: block;
+        padding: 5px 10px;
+        border-radius: 5px;
+        position: absolute;
+        bottom: -0.7em;
+        z-index: 10;
+        font-size: 0.7rem;
+        background-color: rgba(0, 0, 0, 0.85);
+        color: #fff;
+    }
+`
+
+export default function TechListDisplay({techGroupList}) {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <TechListContainer color={theme.darkest} underlineColor={theme.highlight}>
+            {
+                data && data.map((techGroup, index) => (
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary key={index} id={`panel-${index}`} expandIcon={<ExpandMoreIcon />}>
+                            <TechGroup>
+                                <TechIconWrap>
+                                    { techGroup.tech.map((tech) => (<TechIcon backgroundImage={`./icons/${tech.icon}.png`} iconLabel={tech.name}/>)) }
+                                </TechIconWrap>   
+                            </TechGroup>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <TechGroupDetails>
+                                <TechTitleWrap>
+                                    { techGroup.tech.map((tech) => (<span>{tech.name}</span>)) }
+                                </TechTitleWrap> 
+                                <p>{techGroup.note}</p>  
+                            </TechGroupDetails>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                ))
+            }
+        </TechListContainer>
+    )
+} 
+
+const data = [
+    {
+        tech: [
+            {icon: "html", name: "HTML"},
+            {icon: "css", name: "CSS"},
+            {icon: "javascript", name: "JavaScript"},
+        ],
+        note: "These are fundamental building block of any website. I especially enjoy making fun animations, effects, and interactions with Html Canvas",
+    },
+    {
+        tech: [
+            {icon: "bootstrap", name: "Bootstrap"},
+            {icon: "sass", name: "Sass"},
+            {icon: "styled-components", name: "Styled-Components"}
+        ],
+        note: "For styling, I have experience using many CSS extensions or libraries such Sass and Bootstrap. However, most of my projects use Styled-Components, CSS modules or just MaterialUI components if I want something done fast.",
+    },
+    {
+        tech: [
+            {icon: "react", name: "React"},
+            {icon: "redux", name: "Redux"},
+            {icon: "mui", name: "Material-UI"},
+        ],
+        note: "",
+    },
+    {
+        tech: [
+            {icon: "node", name: "NodeJS"},
+            {icon: "Mongodb", name: "MongoDB"},
+        ],
+        note: "Node, express, MongoDB is my stack to make RESTful Backends. At the moment, I am hosting my servers on AWS-Ec2 and my database on their instances or MongoDB's cloud clusters.",
+    },
+    {
+        tech: [
+            {icon: "npm", name: "NPM"},
+            {icon: "Webpack", name: "Webpack"},
+            {icon: "git", name: "Git Version Control"},
+            {icon: "VS-Code", name: "VS-Code"},
+            {icon: "jQuery", name: "jQuery"},
+            {icon: "Figma", name: "Figma"},
+            {icon: "Affinity", name: "Affinity Photo"},
+            {icon: "aws", name: "AWS-EC2"},
+        ],
+        note: "These are some of the other tools and technologies I use.",
+    },
+    
+]
